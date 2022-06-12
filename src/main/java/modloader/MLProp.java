0978fe890a1c4e76f1190245de1abd53334b4e31
Caroline Joy Bell <carolinejoybell@gmail.com>
2022-06-11 21:31:41 -0700

    MLProp javadocs cleanup

--- src/main/java/modloader/MLProp.java
+++ src/main/java/modloader/MLProp.java
@@ -10,25 +10,21 @@ import java.lang.annotation.Target;
 public @interface MLProp {
 	/**
 	 * Overrides the field name for property key.
-	 * @return
 	 */
 	String name() default "";
 	
 	/**
 	 * Adds additional help to top of configuration file.
-	 * @return
 	 */
 	String info() default "";
 	
 	/**
 	 * Minimum value allowed if field is a number.
-	 * @return
 	 */
 	double min() default Double.NEGATIVE_INFINITY;
 	
 	/**
 	 * Maximum value allowed if field is a number.
-	 * @return
 	 */
 	double max() default Double.POSITIVE_INFINITY;
 }
