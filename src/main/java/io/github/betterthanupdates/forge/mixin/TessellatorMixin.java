0f7897acefd49544e6d86541eed3cd0894ed28e3
Caroline Joy Bell <carolinejoybell@gmail.com>
2022-06-18 13:44:04 -0700

    Revert "More client patches"
    This reverts commit 5cd7228da41400bc6432ec487b2052a0d52d5bee.

--- src/main/java/io/github/betterthanupdates/forge/mixin/TessellatorMixin.java
+++
@@ -1,8 +0,0 @@
-package io.github.betterthanupdates.forge.mixin;
-
-import net.minecraft.client.render.Tessellator;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(Tessellator.class)
-public class TessellatorMixin {
-}
