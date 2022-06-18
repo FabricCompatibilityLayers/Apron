8abeba78134b6142d40488fa6a62d4423ec1feae
Caroline Joy Bell <carolinejoybell@gmail.com>
2022-06-18 13:44:04 -0700

    Revert "Done porting Client Forge patches"
    This reverts commit fe9ca2bee06a2b94fea149900feb957fb442859e.

--- src/main/java/io/github/betterthanupdates/forge/ForgeItem.java
+++
@@ -1,8 +0,0 @@
-package io.github.betterthanupdates.forge;
-
-import net.minecraft.block.Block;
-import net.minecraft.item.ItemStack;
-
-public interface ForgeItem {
-    float getStrVsBlock(ItemStack itemstack, Block block, int md);
-}
--- src/main/java/io/github/betterthanupdates/forge/ForgeWorld.java
+++
@@ -1,5 +0,0 @@
-package io.github.betterthanupdates.forge;
-
-public interface ForgeWorld {
-    boolean isBlockSolidOnSide(int i, int j, int k, int side);
-}
