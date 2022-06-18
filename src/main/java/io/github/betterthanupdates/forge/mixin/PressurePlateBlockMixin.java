fa5c8c06b699a6717065f17383371bfcffebad43
Caroline Joy Bell <carolinejoybell@gmail.com>
2022-06-18 02:48:49 -0700

    Proper PressurePlateBlockMixin

--- src/main/java/io/github/betterthanupdates/forge/mixin/PressurePlateBlockMixin.java
+++ src/main/java/io/github/betterthanupdates/forge/mixin/PressurePlateBlockMixin.java
@@ -6,35 +6,39 @@ import net.minecraft.block.PressurePlateBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PressurePlateBlock.class)
 public class PressurePlateBlockMixin extends Block {
-	protected PressurePlateBlockMixin(int i, Material arg) {
-		super(i, arg);
+	protected PressurePlateBlockMixin(int blockId, Material material) {
+		super(blockId, material);
 	}
 
 	/**
-	 * @author Forge
-	 * @reason
+	 * @author Eloraam
+	 * @reason Minecraft Forge patch to method
 	 */
-	@Overwrite
-	public boolean canPlaceAt(World world, int i, int j, int k) {
-		return ((ForgeWorld)world).isBlockSolidOnSide(i, j - 1, k, 1);
+	@Inject(method = "canPlaceAt", at = @At("RETURN"), cancellable = true)
+	private void forge$canPlaceAt(final World world, final int x, final int y, final int z,
+	                              final CallbackInfoReturnable<Boolean> cir) {
+		cir.setReturnValue(((ForgeWorld)world).isBlockSolidOnSide(x, y - 1, z, 1));
 	}
 
 	/**
-	 * @author Forge
-	 * @reason
+	 * @author Eloraam
+	 * @reason Minecraft Forge patch to method
 	 */
-	@Overwrite
-	public void onAdjacentBlockUpdate(World world, int i, int j, int k, int l) {
-		boolean flag = !((ForgeWorld)world).isBlockSolidOnSide(i, j - 1, k, 1);
-
-		if (flag) {
-			this.drop(world, i, j, k, world.getBlockMeta(i, j, k));
-			world.setBlock(i, j, k, 0);
+	@Inject(method = "onAdjacentBlockUpdate", at = @At("RETURN"))
+	private void forge$onAdjacentBlockUpdate(final World world, final int x, final int y, final int z, final int side,
+	                                         final CallbackInfo ci) {
+		if (!((ForgeWorld)world).isBlockSolidOnSide(x, y - 1, z, 1)) {
+			this.drop(world, x, y, z, world.getBlockMeta(x, y, z));
+			world.setBlock(x, y, z, 0);
 		}
-
 	}
+
+
 }
