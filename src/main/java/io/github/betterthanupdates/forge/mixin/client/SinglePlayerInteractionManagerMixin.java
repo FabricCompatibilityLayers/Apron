67c9755399755258d2b62b2e59e11e7aae808577
Cat Core <34719527+arthurbambou@users.noreply.github.com>
2022-06-21 16:32:11 +0200

    Add back forge hook.

--- src/main/java/io/github/betterthanupdates/forge/mixin/client/SinglePlayerInteractionManagerMixin.java
+++ src/main/java/io/github/betterthanupdates/forge/mixin/client/SinglePlayerInteractionManagerMixin.java
@@ -5,11 +5,17 @@ import net.fabricmc.api.Environment;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.block.Block;
 import net.minecraft.client.ClientInteractionManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.SingleplayerInteractionManager;
+import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
 
 import io.github.betterthanupdates.forge.block.ForgeBlock;
 
@@ -39,6 +45,19 @@ public class SinglePlayerInteractionManagerMixin extends ClientInteractionManage
 
 	public SinglePlayerInteractionManagerMixin(Minecraft client) {
 		super(client);
+	}
+
+	@Unique
+	int cachedMeta = 0;
+	@Inject(method = "method_1716", at = @At("HEAD"))
+	private void forge$method_1716(int j, int k, int l, int par4, CallbackInfoReturnable<Boolean> cir) {
+		this.cachedMeta = this.client.world.getBlockMeta(j, k, l);
+	}
+
+	@Redirect(method = "method_1716", at = @At(value = "INVOKE",
+			target = "Lnet/minecraft/client/entity/player/AbstractClientPlayerEntity;canRemoveBlock(Lnet/minecraft/block/Block;)Z"))
+	private boolean forge$method_1716(AbstractClientPlayerEntity instance, Block block) {
+		return ((ForgeBlock) block).canHarvestBlock(instance, this.cachedMeta);
 	}
 
 	/**
