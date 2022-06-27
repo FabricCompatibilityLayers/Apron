bb00ecef318e7378b4693fb5e9295d7fd5ed4f7e
Cat Core <34719527+arthurbambou@users.noreply.github.com>
2022-06-27 20:03:23 +0200

    button block

--- src/main/java/io/github/betterthanupdates/forge/mixin/ButtonBlockMixin.java
+++ src/main/java/io/github/betterthanupdates/forge/mixin/ButtonBlockMixin.java
@@ -17,47 +17,79 @@ public abstract class ButtonBlockMixin extends Block {
 		super(blockId, material);
 	}
 
+	/**
+	 * @author Eloraam
+	 * @reason implement Forge hooks
+	 */
 	@Redirect(method = {"canPlaceAt(Lnet/minecraft/world/World;IIII)Z", "onBlockPlaced"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;canSuffocate(III)Z", ordinal = 0))
-	private boolean reforged$canPlaceAt$1(World instance, int j, int k, int i) {
+	private boolean forge$canPlaceAt$1(World instance, int j, int k, int i) {
 		return ((ForgeWorld) instance).isBlockSolidOnSide(i, j, k, 2);
 	}
 
+	/**
+	 * @author Eloraam
+	 * @reason implement Forge hooks
+	 */
 	@Redirect(method = {"canPlaceAt(Lnet/minecraft/world/World;IIII)Z", "onBlockPlaced"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;canSuffocate(III)Z", ordinal = 1))
-	private boolean reforged$canPlaceAt$2(World instance, int j, int k, int i) {
+	private boolean forge$canPlaceAt$2(World instance, int j, int k, int i) {
 		return ((ForgeWorld) instance).isBlockSolidOnSide(i, j, k, 3);
 	}
 
+	/**
+	 * @author Eloraam
+	 * @reason implement Forge hooks
+	 */
 	@Redirect(method = {"canPlaceAt(Lnet/minecraft/world/World;IIII)Z", "onBlockPlaced"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;canSuffocate(III)Z", ordinal = 2))
-	private boolean reforged$canPlaceAt$3(World instance, int j, int k, int i) {
+	private boolean forge$canPlaceAt$3(World instance, int j, int k, int i) {
 		return ((ForgeWorld) instance).isBlockSolidOnSide(i, j, k, 4);
 	}
 
+	/**
+	 * @author Eloraam
+	 * @reason implement Forge hooks
+	 */
 	@Redirect(method = {"canPlaceAt(Lnet/minecraft/world/World;IIII)Z", "onBlockPlaced"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;canSuffocate(III)Z", ordinal = 3))
-	private boolean reforged$canPlaceAt$4(World instance, int j, int k, int i) {
+	private boolean forge$canPlaceAt$4(World instance, int j, int k, int i) {
 		return ((ForgeWorld) instance).isBlockSolidOnSide(i, j, k, 5);
 	}
 
+	/**
+	 * @author Eloraam
+	 * @reason implement Forge hooks
+	 */
 	@Redirect(method = {"canPlaceAt(Lnet/minecraft/world/World;III)Z", "method_1047", "onAdjacentBlockUpdate"},
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;canSuffocate(III)Z", ordinal = 0))
-	private boolean reforged$canPlaceAt$5(World instance, int j, int k, int i) {
+	private boolean forge$canPlaceAt$5(World instance, int j, int k, int i) {
 		return ((ForgeWorld) instance).isBlockSolidOnSide(i, j, k, 5);
 	}
 
+	/**
+	 * @author Eloraam
+	 * @reason implement Forge hooks
+	 */
 	@Redirect(method = {"canPlaceAt(Lnet/minecraft/world/World;III)Z", "method_1047", "onAdjacentBlockUpdate"},
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;canSuffocate(III)Z", ordinal = 1))
-	private boolean reforged$canPlaceAt$6(World instance, int j, int k, int i) {
+	private boolean forge$canPlaceAt$6(World instance, int j, int k, int i) {
 		return ((ForgeWorld) instance).isBlockSolidOnSide(i, j, k, 4);
 	}
 
+	/**
+	 * @author Eloraam
+	 * @reason implement Forge hooks
+	 */
 	@Redirect(method = {"canPlaceAt(Lnet/minecraft/world/World;III)Z", "method_1047", "onAdjacentBlockUpdate"},
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;canSuffocate(III)Z", ordinal = 2))
-	private boolean reforged$canPlaceAt$7(World instance, int j, int k, int i) {
+	private boolean forge$canPlaceAt$7(World instance, int j, int k, int i) {
 		return ((ForgeWorld) instance).isBlockSolidOnSide(i, j, k, 3);
 	}
 
+	/**
+	 * @author Eloraam
+	 * @reason implement Forge hooks
+	 */
 	@Redirect(method = {"canPlaceAt(Lnet/minecraft/world/World;III)Z", "method_1047", "onAdjacentBlockUpdate"},
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;canSuffocate(III)Z", ordinal = 3))
-	private boolean reforged$canPlaceAt$8(World instance, int j, int k, int i) {
+	private boolean forge$canPlaceAt$8(World instance, int j, int k, int i) {
 		return ((ForgeWorld) instance).isBlockSolidOnSide(i, j, k, 2);
 	}
 }
