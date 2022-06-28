bd69e2bb9734dfef5f7581171eb33732a16b8aef
Cat Core <34719527+arthurbambou@users.noreply.github.com>
2022-06-28 22:20:57 +0200

    Be sure mods are correctly remapped to our class overwrites...

--- src/main/java/io/github/betterthanupdates/apron/ApronPostRemappingVisitor.java
+++ src/main/java/io/github/betterthanupdates/apron/ApronPostRemappingVisitor.java
@@ -1,10 +1,14 @@
 package io.github.betterthanupdates.apron;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.tinyremapper.TinyRemapper;
 import net.fabricmc.tinyremapper.api.TrClass;
+import org.objectweb.asm.AnnotationVisitor;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.TypePath;
 
 public class ApronPostRemappingVisitor implements TinyRemapper.ApplyVisitorProvider {
 	@Override
@@ -12,10 +16,93 @@ public class ApronPostRemappingVisitor implements TinyRemapper.ApplyVisitorProvi
 		final String className = cls.getName();
 		return new ClassVisitor(Opcodes.ASM9, next) {
 			@Override
+			public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+				if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+					switch (superName) {
+						case "net/minecraft/class_632":
+							superName = "io/github/betterthanupdates/shockahpi/item/ShockAhPIToolItem";
+							break;
+						case "net/minecraft/class_420":
+							superName = "io/github/betterthanupdates/shockahpi/item/ShockAhPIAxeItem";
+							break;
+						case "net/minecraft/class_116":
+							superName = "io/github/betterthanupdates/shockahpi/item/ShockAhPIPickaxeItem";
+							break;
+						case "net/minecraft/class_501":
+							superName = "io/github/betterthanupdates/shockahpi/item/ShockAhPIShovelItem";
+							break;
+					}
+				}
+
+				super.visit(version, access, name, signature, superName, interfaces);
+			}
+
+			@Override
+			public AnnotationVisitor visitTypeAnnotation(int typeRef, TypePath typePath, String descriptor, boolean visible) {
+				if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+					switch (descriptor) {
+						case "net/minecraft/class_632":
+							descriptor = "io/github/betterthanupdates/shockahpi/item/ShockAhPIToolItem";
+							break;
+						case "net/minecraft/class_420":
+							descriptor = "io/github/betterthanupdates/shockahpi/item/ShockAhPIAxeItem";
+							break;
+						case "net/minecraft/class_116":
+							descriptor = "io/github/betterthanupdates/shockahpi/item/ShockAhPIPickaxeItem";
+							break;
+						case "net/minecraft/class_501":
+							descriptor = "io/github/betterthanupdates/shockahpi/item/ShockAhPIShovelItem";
+							break;
+					}
+				}
+
+				return super.visitTypeAnnotation(typeRef, typePath, descriptor, visible);
+			}
+
+			@Override
 			public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
 				return new MethodVisitor(Opcodes.ASM9, super.visitMethod(access, name, descriptor, signature, exceptions)) {
 					@Override
+					public void visitTypeInsn(int opcode, String type) {
+						if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+							switch (type) {
+								case "net/minecraft/class_632":
+									type = "io/github/betterthanupdates/shockahpi/item/ShockAhPIToolItem";
+									break;
+								case "net/minecraft/class_420":
+									type = "io/github/betterthanupdates/shockahpi/item/ShockAhPIAxeItem";
+									break;
+								case "net/minecraft/class_116":
+									type = "io/github/betterthanupdates/shockahpi/item/ShockAhPIPickaxeItem";
+									break;
+								case "net/minecraft/class_501":
+									type = "io/github/betterthanupdates/shockahpi/item/ShockAhPIShovelItem";
+									break;
+							}
+						}
+
+						super.visitTypeInsn(opcode, type);
+					}
+
+					@Override
 					public void visitFieldInsn(int opcode, String fieldOwner, String fieldName, String fieldDescriptor) {
+						if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+							switch (fieldOwner) {
+								case "net/minecraft/class_632":
+									fieldOwner = "io/github/betterthanupdates/shockahpi/item/ShockAhPIToolItem";
+									break;
+								case "net/minecraft/class_420":
+									fieldOwner = "io/github/betterthanupdates/shockahpi/item/ShockAhPIAxeItem";
+									break;
+								case "net/minecraft/class_116":
+									fieldOwner = "io/github/betterthanupdates/shockahpi/item/ShockAhPIPickaxeItem";
+									break;
+								case "net/minecraft/class_501":
+									fieldOwner = "io/github/betterthanupdates/shockahpi/item/ShockAhPIShovelItem";
+									break;
+							}
+						}
+
 						switch (fieldOwner) {
 							case "net/minecraft/class_67":
 								if (fieldName.equals("renderingWorldRenderer")) {
@@ -51,6 +138,23 @@ public class ApronPostRemappingVisitor implements TinyRemapper.ApplyVisitorProvi
 
 					@Override
 					public void visitMethodInsn(int opcode, String methodOwner, String methodName, String methodDescriptor, boolean isInterface) {
+						if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+							switch (methodOwner) {
+								case "net/minecraft/class_632":
+									methodOwner = "io/github/betterthanupdates/shockahpi/item/ShockAhPIToolItem";
+									break;
+								case "net/minecraft/class_420":
+									methodOwner = "io/github/betterthanupdates/shockahpi/item/ShockAhPIAxeItem";
+									break;
+								case "net/minecraft/class_116":
+									methodOwner = "io/github/betterthanupdates/shockahpi/item/ShockAhPIPickaxeItem";
+									break;
+								case "net/minecraft/class_501":
+									methodOwner = "io/github/betterthanupdates/shockahpi/item/ShockAhPIShovelItem";
+									break;
+							}
+						}
+
 						switch (methodOwner) {
 							case "net/minecraft/class_13":
 								if (methodName.equals("setRedstoneColors")) {
