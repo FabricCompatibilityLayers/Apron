6e460eb81d963f29345e113a1a38c0d4087e7706
Cat Core <34719527+arthurbambou@users.noreply.github.com>
2022-08-23 01:23:23 +0200

    Start hooking rendering mixins into Arsenic Renderer

---
+++ src/main/java/io/github/betterthanupdates/stapi/MixinPlugin.java
@@ -0,0 +1,46 @@
+package io.github.betterthanupdates.stapi;
+
+import java.util.List;
+import java.util.Set;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+public class MixinPlugin implements IMixinConfigPlugin {
+	@Override
+	public void onLoad(String mixinPackage) {
+
+	}
+
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		return FabricLoader.getInstance().isModLoaded("stationapi") == mixinClassName.contains("stapi.");
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+	}
+
+	@Override
+	public List<String> getMixins() {
+		return null;
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
+}
--- src/main/java/io/github/betterthanupdates/stapi/mixin/client/forge/ArsenicOverlayRendererMixin.java
+++ src/main/java/io/github/betterthanupdates/stapi/mixin/client/forge/ArsenicOverlayRendererMixin.java
@@ -1,12 +1,13 @@
-package io.github.betterthanupdates.forge.mixin.client;
+package io.github.betterthanupdates.stapi.mixin.client.forge;
 
 import forge.ForgeHooksClient;
 import forge.ICustomItemRenderer;
 import forge.MinecraftForgeClient;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
+import net.modificationstation.stationapi.impl.client.arsenic.renderer.render.ArsenicOverlayRenderer;
+import net.modificationstation.stationapi.mixin.arsenic.client.class_556Accessor;
 import org.lwjgl.opengl.GL11;
 import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,27 +15,21 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.render.HeldItemRenderer;
-import net.minecraft.client.render.block.BlockRenderer;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
-@Environment(EnvType.CLIENT)
-@Mixin(HeldItemRenderer.class)
-public class HeldItemRendererMixin {
+@Mixin(ArsenicOverlayRenderer.class)
+public class ArsenicOverlayRendererMixin {
 	@Shadow
-	private Minecraft client;
-
-	@Shadow
-	private BlockRenderer blockRenderer;
+	@Final
+	private class_556Accessor access;
 
 	/**
 	 * @author Eloraam
 	 * @reason implement Forge hooks
 	 */
-	@Inject(method = "render",
+	@Inject(method = "renderItem3D",
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/block/BlockRenderer;method_48(Lnet/minecraft/block/Block;IF)V", ordinal = 0))
 	private void forge$render$1(LivingEntity entityliving, ItemStack itemstack, CallbackInfo ci) {
 		ForgeHooksClient.overrideTexture(Block.BY_ID[itemstack.itemId]);
@@ -44,10 +39,10 @@ public class HeldItemRendererMixin {
 	 * @author Eloraam
 	 * @reason implement Forge hooks
 	 */
-	@Inject(method = "render", at = @At(value = "FIELD", opcode = Opcodes.GETSTATIC,
+	@Inject(method = "renderItem3D", at = @At(value = "FIELD", opcode = Opcodes.GETSTATIC,
 			target = "Lnet/minecraft/client/render/Tessellator;INSTANCE:Lnet/minecraft/client/render/Tessellator;"))
 	private void forge$render$2(LivingEntity arg2, ItemStack itemStack, CallbackInfo ci) {
-		if (itemStack.itemId < 256) {
+		if (itemStack.itemId < Block.BY_ID.length) {
 			ForgeHooksClient.overrideTexture(Block.BY_ID[itemStack.itemId]);
 		} else {
 			ForgeHooksClient.overrideTexture(Item.byId[itemStack.itemId]);
@@ -58,15 +53,15 @@ public class HeldItemRendererMixin {
 	 * @author Eloraam
 	 * @reason implement Forge hooks
 	 */
-	@Inject(method = "render", cancellable = true,
+	@Inject(method = "renderItem3D", cancellable = true,
 			at = @At(value = "INVOKE", ordinal = 0, target = "Lorg/lwjgl/opengl/GL11;glPushMatrix()V", remap = false, shift = At.Shift.AFTER))
 	private void forge$render$3(LivingEntity entityliving, ItemStack itemStack, CallbackInfo ci) {
 		ICustomItemRenderer customRenderer = MinecraftForgeClient.getCustomItemRenderer(itemStack.itemId);
 
 		if (customRenderer != null) {
-			GL11.glBindTexture(3553, this.client.textureManager.getTextureId("/terrain.png"));
+			GL11.glBindTexture(3553, this.access.stationapi$getField_2401().textureManager.getTextureId("/terrain.png"));
 			ForgeHooksClient.overrideTexture(itemStack.getItem());
-			ForgeHooksClient.renderCustomItem(customRenderer, this.blockRenderer, itemStack.itemId, itemStack.getMeta(), entityliving.getBrightnessAtEyes(1.0F));
+			ForgeHooksClient.renderCustomItem(customRenderer, this.access.stationapi$getField_2405(), itemStack.itemId, itemStack.getMeta(), entityliving.getBrightnessAtEyes(1.0F));
 			GL11.glPopMatrix();
 			ci.cancel();
 		}
