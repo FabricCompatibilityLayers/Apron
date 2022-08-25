f0f45558061f4da9f16bb43039d224f358d1c3f5
Cat Core <34719527+arthurbambou@users.noreply.github.com>
2022-08-25 23:41:56 +0200

    Fix debug line when stapi is loaded

--- src/main/java/io/github/betterthanupdates/apron/mixin/client/InGameHudMixin.java
+++ src/main/java/io/github/betterthanupdates/apron/mixin/client/InGameHudMixin.java
@@ -4,6 +4,7 @@ import static io.github.betterthanupdates.apron.Apron.*;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -30,7 +31,13 @@ public abstract class InGameHudMixin extends GuiElement {
 	@Inject(method = "render(FZII)V", at = @At(value = "INVOKE",
 			target = "Lnet/minecraft/util/math/MathHelper;floor(D)I"))
 	private void apron$addDebugLine(float bl, boolean i, int j, int par4, CallbackInfo ci) {
+		int y = 96;
+
+		if (FabricLoader.getInstance().isModLoaded("stationapi")) {
+			y = 106;
+		}
+
 		this.drawTextWithShadow(this.client.textRenderer,
-				String.format("M: %s, %s", rmlModsLoaded(), fabricModsLoaded()), 2, 96, 0xE0_E0_E0);
+				String.format("M: %s, %s", rmlModsLoaded(), fabricModsLoaded()), 2, y, 0xE0_E0_E0);
 	}
 }
