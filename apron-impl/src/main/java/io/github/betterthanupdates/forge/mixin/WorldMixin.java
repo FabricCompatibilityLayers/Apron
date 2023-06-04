e73c8497a2cb2bfbc8c25dca3dc4856c821d188b
VendoAU <reillyreaby@gmail.com>
2023-06-04 13:32:31 +1000

    Forge hooks in World

--- apron-impl/src/main/java/io/github/betterthanupdates/forge/mixin/WorldMixin.java
+++ apron-impl/src/main/java/io/github/betterthanupdates/forge/mixin/WorldMixin.java
@@ -1,47 +1,33 @@
 package io.github.betterthanupdates.forge.mixin;
 
-import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import net.minecraft.block.Block;
 import net.minecraft.util.math.AxixAlignedBoundingBox;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.LightType;
 import net.minecraft.world.World;
-import net.minecraft.world.dimension.Dimension;
 
 import io.github.betterthanupdates.forge.block.ForgeBlock;
 import io.github.betterthanupdates.forge.world.ForgeWorld;
 
+@Debug(export = true)
 @Mixin(World.class)
 public abstract class WorldMixin implements BlockView, ForgeWorld {
-	@Shadow
-	public abstract boolean canSpawnEntity(AxixAlignedBoundingBox box);
-
-	@Shadow
-	public abstract boolean method_155(int i, int j, int k, int l, int m, int n);
-
-	@Shadow
-	@Final
-	public Dimension dimension;
 
 	@Shadow
 	public abstract boolean isBlockLoaded(int i, int j, int k);
 
-	@Shadow
-	public abstract boolean isAboveGround(int i, int j, int k);
-
-	@Shadow
-	public abstract int method_164(LightType arg, int i, int j, int k);
-
-	@Shadow
-	public abstract void method_166(LightType arg, int i, int j, int k, int l, int m, int n);
+	private int light;
+	private boolean replaceable;
 
 	/**
 	 * @author Eloraam
@@ -57,26 +43,16 @@ public abstract class WorldMixin implements BlockView, ForgeWorld {
 	 * @author Eloraam
 	 * @reason implement Forge hooks
 	 */
-	@Overwrite
-	public void method_165(LightType lightType, int x, int y, int z, int l) {
-		if (!this.dimension.halvesMapping || lightType != LightType.field_2757) {
-			if (this.isBlockLoaded(x, y, z)) {
-				if (lightType == LightType.field_2757) {
-					if (this.isAboveGround(x, y, z)) {
-						l = 15;
-					}
-				} else if (lightType == LightType.field_2758) {
-					int i1 = this.getBlockId(x, y, z);
-					int bl = i1 == 0 ? 0 : ((ForgeBlock) Block.BY_ID[i1]).getLightValue(this, x, y, z);
+	@Inject(method = "method_165", at = @At("HEAD"))
+	public void method_165(LightType lightType, int x, int y, int z, int l, CallbackInfo ci) {
+		if (this.isBlockLoaded(x, y, z) && lightType == LightType.field_2758) {
+			int i1 = this.getBlockId(x, y, z);
+			int bl = i1 == 0 ? 0 : ((ForgeBlock) Block.BY_ID[i1]).getLightValue(this, x, y, z);
 
-					if (bl > l) {
-						l = bl;
-					}
-				}
-
-				if (this.method_164(lightType, x, y, z) != l) {
-					this.method_166(lightType, x, y, z, x, y, z);
-				}
+			if (bl > l) {
+				light = bl;
+			} else {
+				light = 0;
 			}
 		}
 	}
@@ -85,34 +61,23 @@ public abstract class WorldMixin implements BlockView, ForgeWorld {
 	 * @author Eloraam
 	 * @reason implement Forge hooks
 	 */
-	@Overwrite
-	public boolean method_225(AxixAlignedBoundingBox box) {
-		int i = MathHelper.floor(box.minX);
-		int j = MathHelper.floor(box.maxX + 1.0);
-		int k = MathHelper.floor(box.minY);
-		int l = MathHelper.floor(box.maxY + 1.0);
-		int i1 = MathHelper.floor(box.minZ);
-		int j1 = MathHelper.floor(box.maxZ + 1.0);
-
-		if (this.method_155(i, k, i1, j, l, j1)) {
-			for (int k1 = i; k1 < j; ++k1) {
-				for (int l1 = k; l1 < l; ++l1) {
-					for (int i2 = i1; i2 < j1; ++i2) {
-						int j2 = this.getBlockId(k1, l1, i2);
-
-						if (j2 == Block.FIRE.id || j2 == Block.FLOWING_LAVA.id || j2 == Block.STILL_LAVA.id) {
-							return true;
-						}
-
-						if (j2 > 0 && ((ForgeBlock) Block.BY_ID[j2]).isBlockBurning((World) (Object) this, k1, l1, i2)) {
-							return true;
-						}
-					}
-				}
-			}
+	@ModifyVariable(method = "method_165", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;method_164(Lnet/minecraft/world/LightType;III)I"), argsOnly = true, ordinal = 3)
+	public int method_165(int l) {
+		if (light != 0) {
+			return light;
 		}
+		return l;
+	}
 
-		return false;
+	/**
+	 * @author Eloraam
+	 * @reason implement Forge hooks
+	 */
+	@Inject(method = "method_225", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getBlockId(III)I", shift = At.Shift.BY, by = 4), cancellable = true, locals = LocalCapture.CAPTURE_FAILEXCEPTION)
+	public void method_225(AxixAlignedBoundingBox var1, CallbackInfoReturnable<Boolean> cir, int i, int j, int k, int l, int i1, int j1, int k1, int l1, int i2, int j2) {
+		if (j2 > 0 && ((ForgeBlock) Block.BY_ID[j2]).isBlockBurning((World) (Object) this, k1, l1, i2)) {
+			cir.setReturnValue(true);
+		}
 	}
 
 	/**
@@ -135,34 +100,22 @@ public abstract class WorldMixin implements BlockView, ForgeWorld {
 	 * @author Eloraam
 	 * @reason implement Forge hooks
 	 */
-	@Overwrite
-	public boolean canPlaceBlock(int id, int x, int y, int z, boolean flag, int i1) {
+	@Inject(method = "canPlaceBlock", at = @At("HEAD"))
+	public void canPlaceBlock(int id, int x, int y, int z, boolean flag, int i1, CallbackInfoReturnable<Boolean> cir) {
 		int j1 = this.getBlockId(x, y, z);
 		Block block = Block.BY_ID[j1];
-		Block block1 = Block.BY_ID[id];
-		AxixAlignedBoundingBox box = block1.getCollisionShape((World) (Object) this, x, y, z);
+		replaceable = block != null && ((ForgeBlock) block).isBlockReplaceable((World) (Object) this, x, y, z);
+	}
 
-		if (flag) {
-			box = null;
+	/**
+	 * @author Eloraam
+	 * @reason implement Forge hooks
+	 */
+	@ModifyVariable(method = "canPlaceBlock", index = 8, at = @At(value = "STORE"))
+	public Block canPlaceBlock(Block block) {
+		if (replaceable) {
+			return null;
 		}
-
-		if (box != null && !this.canSpawnEntity(box)) {
-			return false;
-		} else {
-			if (block == Block.FLOWING_WATER
-					|| block == Block.STILL_WATER
-					|| block == Block.FLOWING_LAVA
-					|| block == Block.STILL_LAVA
-					|| block == Block.FIRE
-					|| block == Block.SNOW) {
-				block = null;
-			}
-
-			if (block != null && ((ForgeBlock) block).isBlockReplaceable((World) (Object) this, x, y, z)) {
-				block = null;
-			}
-
-			return id > 0 && block == null && block1.canPlaceAt((World) (Object) this, x, y, z, i1);
-		}
+		return block;
 	}
 }
