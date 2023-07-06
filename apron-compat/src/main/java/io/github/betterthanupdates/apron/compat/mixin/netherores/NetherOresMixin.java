244ae7435e8d94e5ce97c664349ae4d0b9c29615
VendoAU <reillyreaby@gmail.com>
2023-07-07 00:00:05 +1000

    Add Nether Ores Remake compatibility

---
+++ apron-compat/src/main/java/io/github/betterthanupdates/apron/compat/mixin/netherores/NetherOresMixin.java
@@ -0,0 +1,15 @@
+package io.github.betterthanupdates.apron.compat.mixin.netherores;
+
+import net.minecraft.mod_NetherOres;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(mod_NetherOres.class)
+public class NetherOresMixin {
+
+	@ModifyConstant(method = "<init>", remap = false, constant = @Constant(stringValue = "d"))
+	public String init(String s) {
+		return "method_1792";
+	}
+}
