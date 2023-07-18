762859a6265580507310f17fe30d73a3f53864a2
Cat Core <34719527+arthurbambou@users.noreply.github.com>
2023-07-18 23:57:37 +0200

    [StAPI Compat] Fix creative tabs name

--- apron-stapi/src/main/java/io/github/betterthanupdates/apron/stapi/CreativeTabsListener.java
+++ apron-stapi/src/main/java/io/github/betterthanupdates/apron/stapi/CreativeTabsListener.java
@@ -1,8 +1,13 @@
 package io.github.betterthanupdates.apron.stapi;
 
+import java.util.Map;
+
+import modloader.ModLoader;
 import net.mine_diver.unsafeevents.listener.EventListener;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+
+import net.modificationstation.stationapi.api.registry.Identifier;
 import net.modificationstation.stationapi.api.registry.ModID;
 import paulevs.bhcreative.api.SimpleTab;
 import paulevs.bhcreative.registry.TabRegistryEvent;
@@ -10,18 +15,28 @@ import paulevs.bhcreative.registry.TabRegistryEvent;
 public class CreativeTabsListener {
 	@EventListener
 	public void onTabInit(TabRegistryEvent event) {
-//		tab = new SimpleTab(YOUR_MOD_ID.id("yourTabName"), ItemBase.apple); // Making tab
-//		event.register(tab); // Registering tab
-//		tab.addItem(new ItemInstance(ItemBase.apple)); // Adding apple
 		ApronStAPICompat.getModContents().forEach(entry -> {
 			ModID modID = entry.getKey();
 			ModContents modContents = entry.getValue();
 
-			SimpleTab tab = new SimpleTab(modID.id("tab"), Item.DIAMOND);
+			if (!modContents.ITEMS.originalToInstance.isEmpty()) {
+				Item firstItem = Item.DIAMOND;
 
-			event.register(tab);
+				for (Map.Entry<Integer, Item> entry1 : modContents.ITEMS.originalToInstance.entrySet()) {
+					firstItem = entry1.getValue();
+					break;
+				}
 
-			modContents.ITEMS.originalToInstance.forEach((integer, item) -> tab.addItem(new ItemStack(item)));
+				Identifier tabId = modID.id("tab");
+
+				SimpleTab tab = new SimpleTab(tabId, firstItem);
+
+				event.register(tab);
+
+				modContents.ITEMS.originalToInstance.forEach((integer, item) -> tab.addItem(new ItemStack(item)));
+
+				ModLoader.AddLocalization("tab." + tabId + ".name", modID.toString().replace("mod_", ""));
+			}
 		});
 	}
 }
